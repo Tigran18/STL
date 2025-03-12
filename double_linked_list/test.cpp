5b03e44b77480c0bbeb56ddcb2c389e0f8a55148
Tigran18 <tigrandavtyan18@gmail.com>
2025-03-12 23:33:24 +0400

    Operator [] tested.

--- double_linked_list/test.cpp
+++ double_linked_list/test.cpp
@@ -31,6 +31,13 @@ int main(){
     auto end_for_integer2=std::chrono::high_resolution_clock::now();
     auto duration_for_reverse_iterator=std::chrono::duration_cast<std::chrono::nanoseconds>(end_for_integer2-start_for_integer2).count();
     std::cout<<"Duration for reverse iterator: "<<duration_for_reverse_iterator<<std::endl;
-    std::cout<<integer_list[5]<<std::endl;
+    try{
+        std::cout<<integer_list[0]<<std::endl;
+        std::cout<<integer_list[6]<<std::endl;
+        std::cout<<integer_list[-5]<<std::endl;
+    }
+    catch(const std::exception& ex){
+        std::cout<<ex.what()<<std::endl;
+    }
     return 0;
 }
