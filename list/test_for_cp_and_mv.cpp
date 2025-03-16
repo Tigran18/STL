c22b0d5f401cf518af9dcde04ead4815d97bd3f4
Tigran18 <tigrandavtyan18@gmail.com>
2025-03-16 15:56:02 +0400

    Copy and move assignment constructors added for all cuntructors.

---
+++ list/test_for_cp_and_mv.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "list.h"
+
+int main(){
+    my::list<int> l1={7, 8, 9, 10};
+    std::cout<<"l1 list elements are: ";
+    for(auto it=l1.begin(); it!=l1.end(); ++it){
+        std::cout<<*it<<"  ";
+    }
+    my::list<int> l2;
+    l2=std::move(l1);
+    std::cout<<"\nl2 list elements are: ";
+    for(auto it=l2.begin(); it!=l2.end(); ++it){
+        std::cout<<*it<<"  ";
+    }
+    std::cout<<"\nl1 list elements after move are: ";
+    for(auto it=l1.begin(); it!=l1.end(); ++it){
+        std::cout<<*it<<"  ";
+    }
+    std::cout<<std::endl;
+    return 0;
+}
