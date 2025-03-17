5e99f9c228f616f45016dc75ddc2e6f6d85915ee
Original author <tigrandavtyan18@gmail.com>
2025-03-18 00:02:54 +0400

    Simple test added for bubble sort.

--- Algorithms/bubble_sort/bubble_sort.cpp
+++ Algorithms/bubble_sort/bubble_sort.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <list>
+#include <chrono>
 
 void bubble_sort(std::vector<int>& array){
     bool swapped=true;
@@ -21,10 +23,14 @@ int main(){
     for(std::size_t i=0; i<20; i++){
         vec1.push_back(rand()%51-25);
     }
+    auto start1=std::chrono::high_resolution_clock::now();
     bubble_sort(vec1);
     for(auto& el : vec1){
         std::cout<<el<<"  ";
     }
+    auto end1=std::chrono::high_resolution_clock::now();
+    auto duration1=std::chrono::duration_cast<std::chrono::nanoseconds>(end1-start1).count();
+    std::cout<<"\nTime to sort "<<typeid(vec1).name()<<" of size "<<vec1.size()<<" is "<<duration1<<" nanoseconds"<<std::endl;
     std::cout<<std::endl;
     return 0;
 }
