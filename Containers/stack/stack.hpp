b99a571c805aede38698a890b06da328393c65d3
Original author <tigrandavtyan18@gmail.com>
2025-03-18 23:40:29 +0400

    More improvements are done. Move and copy assignments constructor added.

--- Containers/stack/stack.hpp
+++ Containers/stack/stack.hpp
@@ -1,51 +1,65 @@
 #pragma once
-#include <memory>
-#include <utility>
 #include <vector>
 #include <initializer_list>
+#include <stdexcept>
 
-namespace my{
+namespace my {
     template <typename T>
-    class stack{
-        private:
+    class stack {
+    private:
         std::vector<T> m_stack;
-        std::size_t m_size=0;
+        std::size_t m_size = 0;
 
-        public:
-        stack():m_stack({}), m_size(0){}
+    public:
+        stack() = default;
 
-        stack(std::initializer_list<T> init):stack(){
-            for(auto& arg : init){
-                m_stack.push_back(arg);
-                ++m_size;
+        stack(std::initializer_list<T> init) : m_stack(init), m_size(init.size()) {}
+
+        stack(const stack& other) = default;
+
+        stack(stack&& other) noexcept : m_stack(std::move(other.m_stack)), m_size(other.m_size) {
+            other.m_size = 0;
+        }
+
+        stack& operator=(const stack& other) {
+            if (this != &other) {
+                m_stack = other.m_stack;
+                m_size = other.m_size;
             }
+            return *this;
         }
 
-        stack(const stack& other):m_stack(other.m_stack), m_size(other.m_size){
-        }
-
-        stack(stack&& other)noexcept:m_stack(other.m_stack), m_size(other.m_size){
-            while(other.m_stack.size()!=0){
-                other.top();
+        stack& operator=(stack&& other) noexcept {
+            if (this != &other) {
+                m_stack = std::move(other.m_stack);
+                m_size = other.m_size;
+                other.m_size = 0;
             }
-            other.m_size=0;
+            return *this;
         }
 
-        void push_back(T value){
+        void push(const T& value) {
             m_stack.push_back(value);
             ++m_size;
         }
 
-        T top(){
-            if(m_size!=0){
-                T value=m_stack[m_stack.size()-1];
-                m_stack.erase(m_stack.begin()+m_stack.size()-1);
-                --m_size;
-                return value;
-            }
-            else{
-                throw std::out_of_range("No element.");
-            }
+        void pop() {
+            if (m_size == 0) throw std::out_of_range("No element.");
+            m_stack.pop_back();
+            --m_size;
+        }
+
+        T top() const {
+            if (m_size == 0) throw std::out_of_range("No element.");
+            return m_stack.back();
+        }
+
+        std::size_t size() const {
+            return m_size;
+        }
+
+        bool empty() const {
+            return m_size == 0;
         }
     };
 }
