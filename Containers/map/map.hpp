61a5ef32a93b45e1f8991a50eb56af6d8d89fac6
Tigran18 <tigrandavtyan18@gmail.com>
2025-03-26 14:22:14 +0400

    Copy and move constructors were added.

--- Containers/map/map.hpp
+++ Containers/map/map.hpp
@@ -7,7 +7,7 @@ namespace my{
     template <typename T, typename U>
     class map{
         private:
-        std::vector<std::pair<T, U>> m_pairs;
+        std::vector<std::pair<T, U>> m_pairs={};
         public:
         map():m_pairs({}){}
 
@@ -19,6 +19,39 @@ namespace my{
 
         std::size_t size() const {
             return m_pairs.size();
+        }
+
+        map(const map& other)=default;
+
+        map(map&& other)noexcept{
+            m_pairs=std::move(other.m_pairs);
+        }
+
+        map& operator=(const map& other){
+            if(this!=&other){
+                m_pairs.clear();
+                for(auto& pair : other.m_pairs){
+                    m_pairs.push_back(pair);
+                }
+            }
+            return *this;
+        }
+
+        map& operator=(map&& other)noexcept{
+            if(this!=&other){
+                m_pairs=std::move(other.m_pairs);
+            }
+            return *this;
+        }
+
+        U& operator[](const T& key){
+            for(auto& pair : m_pairs){
+                if(pair.first==key){
+                    return pair.second;
+                }
+            }
+            m_pairs.push_back({key, U{}});
+            return m_pairs.back().second;
         }
     };
 }
--- Containers/map/test.cpp
+++ Containers/map/test.cpp
@@ -3,6 +3,6 @@
 
 int main(){
     my::map<int, double> m1={{4, 7.02}, {1, 7.84}};
-    std::cout<<m1.size()<<std::endl;
+    
     return 0;
 }
