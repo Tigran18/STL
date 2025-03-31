26cf56a0384260bf8f7cf5448090eaa53f98be2a
Tigran18 <tigrandavtyan18@gmail.com>
2025-03-31 23:49:41 +0400

    More methods were added for set class.

--- Containers/set/set.hpp
+++ Containers/set/set.hpp
@@ -30,6 +30,38 @@ namespace my{
             
             auto end()const;
 
+            bool empty() const{
+                return m_set.empty();
+            }
+
+            auto find(const T& data)const{
+                for(auto it=m_set.begin(); it!=m_set.end(); ++it){
+                    if(data==*it){
+                        return it;
+                    }
+                }
+                return m_set.end();
+            }
+
+            std::size_t size()const{
+                return m_set.size();
+            }
+
+            void insert(const T& data){
+                m_set.push_back(data);
+                std::sort(m_set.begin(), m_set.end());
+            }
+
+            void erase(const T& data){
+                std::size_t i=0;
+                for(auto it=m_set.begin(); it!=m_set.end(); ++it){
+                    if(*it==data){
+                        m_set.erase(m_set.begin()+i);
+                    }
+                    i++;
+                }
+            }
+
             friend bool check(const set<T>& obj, T el){
                 for(const auto& item : obj.m_set){
                     if(item == el){
--- Containers/set/test.cpp
+++ Containers/set/test.cpp
@@ -14,6 +14,19 @@ int main(){
     for(auto& el : s1){
         std::cout<<el<<"  ";
     }
+    std::cout<<"Finding the iterator of number 9 in s2: "<<*s2.find(9)<<std::endl;
+    std::cout<<"Inserting 5: "<<std::endl;
+    s2.insert(5);
+    std::cout<<"Printing s1 elements after inserting 5: ";
+    for(auto& el : s2){
+        std::cout<<el<<"  ";
+    }
+    std::cout<<"Erasing 5: "<<std::endl;
+    s2.erase(5);
+    std::cout<<"Printing s1 elements after inserting 5: ";
+    for(auto& el : s2){
+        std::cout<<el<<"  ";
+    }
     std::cout<<std::endl;
     return 0;
 }
