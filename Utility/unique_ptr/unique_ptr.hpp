0d8cd75ce02a7eaf1356c4b26b4a395a40f0f853
Tigran18 <tigrandavtyan18@gmail.com>
2025-04-27 12:26:08 +0400

    unique ptr classes created for arrays

--- Utility/unique_ptr/unique_ptr.hpp
+++ Utility/unique_ptr/unique_ptr.hpp
@@ -36,6 +36,42 @@ namespace my{
                 return ptr+k;
             }
     };
+
+    template <typename T>
+    class unique_ptr<T[]> {
+        private:
+            T* ptr;
+        public:
+            unique_ptr(T* p=nullptr):ptr(p){}
+
+            unique_ptr(const unique_ptr& other)=delete;
+
+            unique_ptr& operator=(const unique_ptr& other)=delete;
+
+            unique_ptr(unique_ptr&& other)noexcept:ptr(other.ptr){
+                other.ptr=nullptr;
+            }
+
+            unique_ptr& operator=(unique_ptr&& other){
+                if(*this!=other){
+                    ptr=other.ptr;
+                    other.ptr=nullptr;
+                }
+                return *this;
+            }
+
+            T& operator*()const{
+                return *ptr;
+            }
+
+            T& operator[](std::size_t k)const {
+                return ptr[k];
+            }
+
+            unique_ptr& operator+(const std::size_t& k)const{
+                return ptr+k;
+            }
+    };
 }
 
 #include "unique_ptr.tpp"
